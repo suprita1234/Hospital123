fcdeed7171173fdcc330c59f7b881a834eb1a54f
DELL <DELL@DESKTOP-U424R0D>
2022-11-08 11:28:55 +0530

    sprint1

---
+++ src/main/java/doctors/Pgm1.java
@@ -0,0 +1,11 @@
+package doctors;
+
+public class Pgm1 {
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+		System.out.println("hiiii");
+
+	}
+
+}
---
+++ src/main/java/doctors/Pgm2.java
@@ -0,0 +1,10 @@
+package doctors;
+
+public class Pgm2 {
+public static void main(String[] args)
+{
+	System.out.println("hello");
+	System.out.println("HIhello");
+}
+}
+
