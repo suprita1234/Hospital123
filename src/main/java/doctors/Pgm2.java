c48eeb8b6b04d0612589b7d26e1ef33527ea0978
HP <HP@Shashidharswamy>
2022-11-13 14:44:04 +0530

    spsanglina

--- src/main/java/doctors/Pgm2.java
+++ src/main/java/doctors/Pgm2.java
@@ -5,11 +5,8 @@ public static void main(String[] args)
 {
 	System.out.println("hello");
 	System.out.println("HIhello");
-	
-	System.out.println("hi");
-	System.out.println("how are you");
-	System.out.println("lead");
-	System.out.println("dd");
+	System.out.println("Chiken keka lead");
+	System.out.println("hi suprita");
 }
 }
 
